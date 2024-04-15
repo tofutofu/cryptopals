9f25f1d4b62e2936a6ec122268494b2746a7d7a8
Hans <hansgeunsmeyer@gmail.com>
2024-04-15 11:05:05 -0400

    Challenge 8

---
+++ challenge_08.py
@@ -0,0 +1,59 @@
+# Detect AES in ECB mode
+
+# You are given a bunch of strings (hexstrings). One of them has been encrypted with AES in ECB mode.
+# Detect which one.
+#
+# Hint: ECB mode is stateless and deterministic. The same 16-byte plaintext block
+# always generates the same 16-byte ciphertext block.
+#
+# Let's just assume that the plaintext was in English. How to tackle this problem then?
+# If there is one string with a duplicate block, then that's our best candidate.
+#
+# If no strings have duplicates, can we then find the target string? Would it make sense
+# to apply a distance metric on the ciphertexts - comparing ciphertext blocks?
+# It took me a while to realize that that doesn't make _much_ sense. It only makes sense
+# as a method to find duplicates...
+#
+# Does it make sense to use the total count of duplicates found in a line as as score
+# to compare lines? Not really...
+
+from typing import Sequence
+
+
+def detect_ECB(
+    lines: Sequence[str | bytes], block_size: int = 16
+) -> tuple[int, str | bytes]:
+    duplicated = []
+    for i, line in enumerate(lines):
+        blocks = to_blocks(line, block_size=block_size)
+        if has_duplicates(blocks):
+            duplicated.append((i, line))
+    if not duplicated:
+        return (-1, "")
+    if len(duplicated) > 1:
+        print(
+            f"Warning: Found {len(duplicated)} strings with duplicated blocks. "
+            "Returning only the first string."
+        )
+    return duplicated[0]
+
+
+def to_blocks(line: str | bytes, block_size: int = 16) -> list[str | bytes]:
+    return [line[i : i + block_size] for i in range(0, len(line), block_size)]
+
+
+def has_duplicates(blocks: list) -> bool:
+    return len(set(blocks)) < len(blocks)
+
+
+if __name__ == "__main__":
+    with open("data/8.txt") as f:
+        lines = f.readlines()
+    lines = [line.strip() for line in lines if line.strip()]
+
+    lineno, line = detect_ECB(lines, block_size=32)
+    if lineno == -1:
+        print("Found no strings containing duplicated 16-byte blocks")
+    else:
+        print(f"The line that most likely used AES-ECB is line {lineno}.")
+        print(f"Ciphertext: {line!r}")
